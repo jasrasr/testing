067e7a56f3b5f8b44a225690752d23e78a82ab91
jasrasr <92162022+jasrasr@users.noreply.github.com>
2024-11-04 14:19:35 -0500

    Auto-sync commit 2024-11-04 14:19:21

--- install-git.ps1
+++
@@ -1,7 +0,0 @@
-﻿# Install Git using winget
-Write-Output "Installing Git..."
-winget install --id Git.Git -e --source winget
-
-# Confirm installation
-git --version
-Write-Output "Git has been installed successfully."
